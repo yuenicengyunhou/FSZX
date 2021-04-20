b6bd459f1073ae16a2c161d3c8d8ace940bde1ec
yuenicengyunhou <1751539011@qq.com>
2021-04-20 23:05:23 +0800

    list

--- app/src/main/java/com/itsv/FSZHZX/ui/activity/QuizListActivity.java
+++ app/src/main/java/com/itsv/FSZHZX/ui/activity/QuizListActivity.java
@@ -1,10 +1,25 @@
 package com.itsv.FSZHZX.ui.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.gson.Gson;
 import com.itsv.FSZHZX.R;
+import com.itsv.FSZHZX.api.UserApi;
+import com.itsv.FSZHZX.base.ApiHelper;
+import com.itsv.FSZHZX.base.Constant;
+import com.itsv.FSZHZX.model.QuizModel;
+import com.itsv.FSZHZX.utils.ToastUtils;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class QuizListActivity extends AppCompatActivity {
 
@@ -12,5 +27,41 @@ public class QuizListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_list);
+    }
+
+    public void getRoundQuestion() {
+        UserApi api = ApiHelper.getInstance().buildRetrofit(Constant.questionURL)
+                .createService(UserApi.class);
+        Call<ResponseBody> call = api.getRoundQuestion(Constant.TOKEN);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    try {
+                        String params = response.body().string();
+                        Gson gson = new Gson();
+                        QuizModel model = gson.fromJson(params, QuizModel.class);
+                        if (model != null) {
+                            String msg = model.getMsg();
+                            int code = model.getCode();
+                            if (code != 200) {
+                                ToastUtils.showSingleToast("错误");
+                                return;
+                            }
+                            List<QuizModel.DataBean> data = model.getData();
+                            if (data.isEmpty()) return;
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
+
+            }
+        });
     }
 }
